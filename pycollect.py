bb645af88e44c96d61e6f6368c4afcea95fa2fb0
Sebastien Bariteau <numkem@gmail.com>
2015-09-06 21:15:49 -0400

    Migrated from MongoDB to tinydb. Exception cleanup
    Instead of relying on ValueError to see if a command isn't found, we
    implicitly look to find it ourselves and raise an error in needed.

--- pycollect.py
+++ pycollect.py
@@ -4,33 +4,33 @@ from prompt_toolkit.shortcuts import get_input
 from prompt_toolkit.history import History
 from prompt_toolkit.contrib.completers import WordCompleter
 from pluginbase import PluginBase
-from pymongo import MongoClient
+from tinydb import TinyDB
 import click
 import sys
 import os
 
 from lib.command import UsageException
+from lib.command import Command
 
 
 DEFAULT_DB_FILENAME = os.path.join(os.getcwd(), 'pycollect.db')
 PLUGIN_DIR = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'plugins')
 
+db = TinyDB(DEFAULT_DB_FILENAME)
 
-@click.option('--db', 'db_filename', default=DEFAULT_DB_FILENAME,
-              help="Database filename")
+def run(command, text):
+    command.parse(text)
+    command.run()
+
 @click.command()
-def main(db_filename):
+def main():
     plugin_base = PluginBase(package="pycollect.plugins")
     plugin_source = plugin_base.make_plugin_source(
         searchpath=[PLUGIN_DIR])
     history = History()
     all_commands = dict()
     all_shortcuts = dict()
-
-    # Initialize database
-    client = MongoClient()
-    db = client['pycollect']
 
     # Load all plugins
     with plugin_source:
@@ -43,22 +43,21 @@ def main(db_filename):
                 all_commands.update(cmd.commands)
                 all_shortcuts.update(cmd.shortcuts)
     while True:
-        try:
-            text = get_input(u'> ', history=history,
-                             completer=WordCompleter(all_commands.keys()))
-            try:
-                command = all_commands.get(text.split(' ')[0])()
-            except TypeError:
-                # Try to use a shortcut instead
-                command = all_shortcuts.get(text.split(' ')[0])()
+        text = get_input(u'> ', history=history,
+                         completer=WordCompleter(all_commands.keys()))
 
-            command.parse(text, db=db)
-            try:
-                command.run()
-            except (IndexError, KeyError):
-                raise UsageException()
-        except TypeError:
-            print("Error: Command not found!")
+        cmd_name = text.split(' ')[0]
+        command = None
+        if cmd_name in all_commands:
+            command = all_commands[cmd_name]
+        elif cmd_name in all_shortcuts:
+            command = all_shortcuts[cmd_name]
+        else:
+            Command.error("Command not found.")
+
+        try:
+            if command is not None:
+                run(command(), text)
         except UsageException:
             command.help()
         except KeyboardInterrupt:
